d9f06c94e4a1d0a6d1a30a3b0f71e978356586a5
Marco Gerardo Serrano Lopez <77939514+MarckXsX@users.noreply.github.com>
2023-02-27 18:40:03 -0600

    Update Correo.php
    Seguridad

--- Investigacion 1/Correo.php
+++ Investigacion 1/Correo.php
@@ -23,8 +23,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'tekmo120@gmail.com';                     //SMTP username
-    $mail->Password   = 'fldafntgljkykvgp';                               //SMTP password
+    $mail->Username   = '--@gmail.com';                     //SMTP username
+    $mail->Password   = 'passsword';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
